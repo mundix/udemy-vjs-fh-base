099635aa4ffa476ba0206ed5711d988c47c3a192
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-21 07:17:14 -0400

    Trabajando con axios y giphy api

--- src/bases/11-fetch.js
+++ src/bases/11-fetch.js
@@ -14,7 +14,7 @@ fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
     // console.log(data)
     const {url} = data.images.original
     const img = document.createElement('img')
-    img.src = url;
+    img.src = url
     document.body.append(img)
 })
 .catch(err => {
