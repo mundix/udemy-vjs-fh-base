660b4d833b2a8940cd63b6a7197d1953b77edf7c
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-21 07:34:43 -0400

    Trabajando con async and await

---
+++ src/bases/12-axios.js
@@ -0,0 +1,31 @@
+//Axios npm  
+//Axios tiene varias cosas que el fetch no tiene
+// para instalar axios 
+// $ npm i axios
+
+import axios from 'axios'
+
+const apiKey = '63J5cK4xMY25TfnCcrJzJJn6W06Ed4Xc'
+//https://api.giphy.com/v1/gifs/random?api_key=63J5cK4xMY25TfnCcrJzJJn6W06Ed4Xc
+
+//esto sirve para crear nueva instancia de axios
+const giphyApi = axios.create({
+    //no lleva / al afinal 
+    baseURL: 'https://api.giphy.com/v1/gifs',
+    params: {
+        api_key: apiKey,
+    }
+}) 
+
+giphyApi.get('random')
+    .then( resp => {
+        // console.log(resp.data.data)
+        // const data = resp.data.data 
+        // console.log(data.images.original)
+
+        const { data } = resp.data
+        const { url } = data.images.original
+        const img = document.createElement('img')
+        img.src = url
+        document.body.append(img)
+    })
