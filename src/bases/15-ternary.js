92eeb5d1db9f1345c6fea18291f0df51a8ca28b6
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-08-21 12:48:19 -0400

    Trabajando con Ternary operator

---
+++ src/bases/15-ternary.js
@@ -0,0 +1,22 @@
+//Operador Tenario 
+
+let firstName
+let lastName = 'Pichardo'
+
+console.log(`${firstName || 'No FirstName'} ${lastName || 'No Last Name'}`)
+
+//los ternarios 
+const isActive = true
+
+let message = (isActive === true) ? 'Active' : 'Inactivo' 
+
+// if(isActive) {
+//     message = 'Activo'
+// } else {
+//     message = 'Inactivo'
+// }
+
+console.log(message)
+
+
+
